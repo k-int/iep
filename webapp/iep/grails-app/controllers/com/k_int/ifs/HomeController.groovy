aad187da286dd0c525253f9a315df7287931d135
Ian Ibbotson <ianibbo@googlemail.com>
2011-04-27 16:33:36 +0100

    Cleaned up

--- webapp/iep/grails-app/controllers/com/k_int/ifs/HomeController.groovy
+++ webapp/iep/grails-app/controllers/com/k_int/ifs/HomeController.groovy
@@ -1,12 +1,14 @@
 package com.k_int.ifs
 
 import org.apache.shiro.SecurityUtils
+import com.k_int.iep.datamodel.*
 
 class HomeController {
 
     def index = { 
       println "Home"
-      def subject = SecurityUtils.getSubject()
-      println "Hello ${subject.principal} - auth? ${subject.authenticated}"
+      // def subject = SecurityUtils.getSubject()
+      // println "Hello ${subject.principal} - auth? ${subject.authenticated} - injected: ${authenticatedUser}"
+      println "User: ${authenticatedUser.username}"
     }
 }

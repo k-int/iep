7c4eb96bd05ed3c5a272eba5c10c6f3fb43c41a5
Ian Ibbotson <ianibbo@googlemail.com>
2011-04-27 16:11:38 +0100

    Report userid in controller

--- webapp/iep/grails-app/controllers/com/k_int/ifs/HomeController.groovy
+++ webapp/iep/grails-app/controllers/com/k_int/ifs/HomeController.groovy
@@ -1,8 +1,12 @@
 package com.k_int.ifs
 
+import org.apache.shiro.SecurityUtils
+
 class HomeController {
 
     def index = { 
       println "Home"
+      def subject = SecurityUtils.getSubject()
+      println "Hello ${subject.principal} - auth? ${subject.authenticated}"
     }
 }

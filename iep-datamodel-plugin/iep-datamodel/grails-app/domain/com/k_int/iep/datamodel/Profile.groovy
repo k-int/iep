8c4aae0ee765d00819fdb07a5865e6ca1d27ec9c
Ian Ibbotson <ianibbo@googlemail.com>
2011-05-04 11:27:27 +0100

    updated

--- iep-datamodel-plugin/iep-datamodel/grails-app/domain/com/k_int/iep/datamodel/Profile.groovy
+++
@@ -1,8 +0,0 @@
-
-package com.k_int.iep.datamodel
-
-class Profile extends grails.plugins.nimble.core.ProfileBase {
-
-	// Extend ProfileBase with your custom values here
-
-}
--- iep-datamodel-plugin/iep-datamodel/grails-app/domain/com/k_int/iep/datamodel/User.groovy
+++
@@ -1,8 +0,0 @@
-
-package com.k_int.iep.datamodel
-
-class User extends grails.plugins.nimble.core.UserBase {
-
-	// Extend UserBase with your custom values here
-
-}

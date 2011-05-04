13ddf7d3b2327f721dd58504dce46fc4fa1e342d
Ian Ibbotson <ianibbo@googlemail.com>
2011-05-04 13:03:01 +0100

    Updated

--- iep-datamodel-plugin/iep-datamodel/grails-app/domain/com/k_int/iep/datamodel/IEPConfigParamDefn.groovy
+++ iep-datamodel-plugin/iep-datamodel/grails-app/domain/com/k_int/iep/datamodel/IEPConfigParamDefn.groovy
@@ -2,6 +2,13 @@ package com.k_int.iep.datamodel
 
 class IEPConfigParamDefn {
 
+    String paramNameMessageId
+    String paramType
+
+    IEPConfigParamDefn parent
+
     static constraints = {
+      parent(nullable:true,blank:true)
     }
+
 }
--- iep-datamodel-plugin/iep-datamodel/grails-app/domain/com/k_int/iep/datamodel/IEPProvider.groovy
+++ iep-datamodel-plugin/iep-datamodel/grails-app/domain/com/k_int/iep/datamodel/IEPProvider.groovy
@@ -2,6 +2,34 @@ package com.k_int.iep.datamodel
 
 class IEPProvider {
 
+    String shortCode
+    String name
+    String office
+    String thoroughfare
+    String dependentThoroughfare
+    String locality
+    String dependentLocality
+    String region
+    String postcode
+    String description
+    String contactEmail
+    String contactTelephone
+    String contactFax
+
     static constraints = {
+      shortCode(nullable:true,blank:true)
+      name(nullable:true,blank:true)
+      office(nullable:true,blank:true)
+      thoroughfare(nullable:true,blank:true)
+      dependentThoroughfare(nullable:true,blank:true)
+      locality(nullable:true,blank:true)
+      dependentLocality(nullable:true,blank:true)
+      region(nullable:true,blank:true)
+      postcode(nullable:true,blank:true)
+      description(nullable:true,blank:true)
+      contactEmail(nullable:true,blank:true)
+      contactTelephone(nullable:true,blank:true)
+      contactFax(nullable:true,blank:true)
     }
+
 }

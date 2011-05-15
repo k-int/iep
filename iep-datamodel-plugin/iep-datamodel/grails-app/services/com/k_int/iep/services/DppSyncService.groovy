aef5ad8bd17ec8c709d66b10bcdac0a2c5a12708
Ian Ibbotson <ianibbo@googlemail.com>
2011-05-15 14:42:43 +0100

    Work on Dpp Sync Service

--- iep-datamodel-plugin/iep-datamodel/grails-app/services/com/k_int/iep/services/DppSyncService.groovy
+++ iep-datamodel-plugin/iep-datamodel/grails-app/services/com/k_int/iep/services/DppSyncService.groovy
@@ -1,10 +1,30 @@
 package com.k_int.iep.services
 
+import java.sql.*
+
 class DppSyncService {
 
     static transactional = true
 
     def serviceMethod() {
+    }
 
+    def syncWithDpp(String appid,
+                    String driver,
+                    String jdbc_connect_string,
+                    String user,
+                    String pass) {
+      println "Attempt direct MySQL connection with ${jdbc_connect_string}"
+      try {
+        Class.forName(driver);
+        Connection con = DriverManager.getConnection(jdbc_connect_string,user,pass);
+        println "Connected"
+        Statement stmt = con.createStatement();
+        stmt.close();
+        con.close();
+      }
+      catch( Exception e ) {
+        e.printStackTrace();        
+      }
     }
 }

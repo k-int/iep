5b7492972463c44e4660709d50245c5dcd3996fa
Ian Ibbotson <ianibbo@googlemail.com>
2011-05-15 15:33:09 +0100

    updated - working test of syncup

--- iep-datamodel-plugin/iep-datamodel/grails-app/services/com/k_int/iep/services/DppSyncService.groovy
+++ iep-datamodel-plugin/iep-datamodel/grails-app/services/com/k_int/iep/services/DppSyncService.groovy
@@ -20,11 +20,11 @@ class DppSyncService {
         Connection con = DriverManager.getConnection(jdbc_connect_string,user,pass);
         println "Connected"
         Statement stmt = con.createStatement();
-        if ( stmt.execute("select ID,EMAIL,FEEDBACK_URL,SOURCE_IDENTIFIER,SOURCE_NAME,RECORD_LICENSE,HAS_ICON,FEEDBACK_NAME,ICON_URL from AGGR_SOURCE") ) {
+        if ( stmt.execute("select ID,EMAIL,FEEDBACK_NAME,FEEDBACK_URL,ICON_URL,SOURCE_IDENTIFIER,SOURCE_NAME,RECORD_LICENSE,SHORT_CODE from AGGR_SOURCE") ) {
           ResultSet rs = stmt.getResultSet()
           rs.beforeFirst();
           while ( rs.next() )  {
-            println "Process entry ${rs.getString(1)} ${rs.getString(2)} ${rs.getString(3)} ${rs.getString(4)} ${rs.getString(5)} ${rs.getString(6)} ${rs.getString(7)} ${rs.getString(8)} ${rs.getString(9)} ${rs.getString(10)}"
+            println "Process entry ${rs.getString(1)} ${rs.getString(2)} ${rs.getString(3)} ${rs.getString(4)} ${rs.getString(5)} ${rs.getString(6)} ${rs.getString(7)} ${rs.getString(8)} ${rs.getString(9)}"
           }
           println "Done"
           rs.close()

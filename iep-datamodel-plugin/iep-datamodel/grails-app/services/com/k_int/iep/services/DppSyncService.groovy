14ea66b2ffbba18d1d4d24b692dd436bdfa2ec81
Ian Ibbotson <ianibbo@googlemail.com>
2011-05-15 13:45:39 +0100

    Created skeletal Dpp Sync Service

---
+++ iep-datamodel-plugin/iep-datamodel/grails-app/services/com/k_int/iep/services/DppSyncService.groovy
@@ -0,0 +1,10 @@
+package com.k_int.iep.services
+
+class DppSyncService {
+
+    static transactional = true
+
+    def serviceMethod() {
+
+    }
+}
---
+++ iep-datamodel-plugin/iep-datamodel/test/unit/com/k_int/iep/services/DppSyncServiceTests.groovy
@@ -0,0 +1,17 @@
+package com.k_int.iep.services
+
+import grails.test.*
+
+class DppSyncServiceTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}

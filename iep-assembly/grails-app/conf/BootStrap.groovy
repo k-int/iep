32c5db4a81ca995bcc86aad8ebe2d0afdfda44de
Ian Ibbotson <ianibbo@googlemail.com>
2011-07-04 13:30:54 +0100

    Working nimble config, with user and role checking in sip plugin

--- iep-assembly/grails-app/conf/BootStrap.groovy
+++ iep-assembly/grails-app/conf/BootStrap.groovy
@@ -1,12 +1,63 @@
+import com.k_int.sim.*;
+import grails.plugins.nimble.InstanceGenerator
+import grails.plugins.nimble.core.LevelPermission
+import grails.plugins.nimble.core.Role
+import grails.plugins.nimble.core.Group
+import grails.plugins.nimble.core.AdminsService
+import grails.plugins.nimble.core.UserService
+
 class BootStrap {
 
-    def ontologyService 
+  def grailsApplication
+  def nimbleService
+  def userService
+  def adminsService
+  def ontologyService 
 
-    def init = { servletContext ->
+  def init = { servletContext ->
 
-      ontologyService.sync();
+    println "BootStrap"
 
+    // The following must be executed before any other nimble code
+    nimbleService.init()
+
+    // Create example Administrative account
+    println "Verifying admin account"
+    def userLookup = User.findByUsername('admin')
+    if ( userLookup == null ) {
+      println "Create admin account..."
+      def admins = Role.findByName(AdminsService.ADMIN_ROLE)
+      def admin = InstanceGenerator.user()
+      admin.username = "admin"
+      admin.pass = "admiN123!"
+      admin.passConfirm = "admiN123!"
+      admin.enabled = true
+
+      def adminProfile = InstanceGenerator.profile()
+      adminProfile.fullName = "Administrator"
+      adminProfile.owner = admin
+      admin.profile = adminProfile
+
+      // userLookup = grails.plugins.nimble.core.UserBase.findByUsername(admin.username)
+      def savedAdmin = userService.createUser(admin)
+
+      if (savedAdmin.hasErrors()) {
+        savedAdmin.errors.each {
+          log.error(it)
+        }
+        throw new RuntimeException("Error creating administrator")
+      }
+
+      adminsService.add(admin)
     }
-    def destroy = {
-    }
+
+
+    println "BootStrap"
+    ontologyService.sync();
+
+  }
+
+  def destroy = {
+  }
+
 }
